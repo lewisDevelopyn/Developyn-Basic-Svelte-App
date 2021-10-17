692488f92fc02bce5761cfe93edfeee069eec6c0
lewis.howl@developyn.com <lewis.howl@developyn.com>
2021-10-17 18:13:54 +0100

    first commit

---
+++ basic_svelte/src/routes/index.svelte
@@ -0,0 +1,18 @@
+<!--Add your html here.-->
+<div>
+	<p>This is the raw html section.</p>
+</div>
+
+<script>
+	// Any JavaScript you would implement in this page would go here, you don't have to enter any here though, JavaScript will be your next module.
+</script>
+
+
+<!-- add your css here.-->
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
